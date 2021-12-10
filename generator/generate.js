889b7741f4aad2ac4a56c1fb1a79c7cabacd9a48
onkeloki <ich@markoluft.de>
2021-12-10 22:22:06 +0100

    map template and number replacer

--- generator/generate.js
+++ generator/generate.js
@@ -1,33 +1,30 @@
-fs = require('fs');
-codes = require("./codes.js");
 
+const fs = require('fs');
+const codes = require("./codes.js");
+const numberReplacer = require('./helpers/numberReplacer.js');
+
+mapTemplate = require('../map.json');
+//console.log(mapTemplate);
 generateCodeGroup(codes.group1);
+
 
 
 
 function generateCodeGroup(codeGroup) {
     codeGroup.forEach((codeItem) => {
+        numberReplacer.replace(codeItem.code, mapTemplate, "456");
+
         writeFile(codeItem.code, codeItem.meaning);
     })
-
 }
 
 
 
 function writeFile(code, meaning) {
-    fs.writeFile('../dist/' + code + '.txt', meaning, function (err) {
+    console.log("writing");
+    fs.writeFile('../dist/' + code + '.json', meaning, function (err) {
         if (err) return console.log(err);
-        console.log('Hello World > helloworld.txt');
+        console.log('create ' + code + '.json');
     });
-
 }
-codes.forEach((code, index) => {
-
-    /*   console.log(code, index)
-      fs.writeFile('../dist/' + code.code + '.txt', code.meaning, function (err) {
-          if (err) return console.log(err);
-          console.log('Hello World > helloworld.txt');
-      }); */
-
-})
 
